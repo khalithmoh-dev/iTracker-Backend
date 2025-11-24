6e820abe24ada4506276169fe9fd0aea4b692d96
khalithmoh <khalithmoh@octoze.com>
2025-11-24 18:20:27 +0530

    cors validatations added

--- src/index.ts
+++ src/index.ts
@@ -26,9 +26,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://itracker-es0n.onrender.com/', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
-
 // Routes
 app.use('/api/auth', authRouter);
 app.use('/api/investments', authMiddleware, investmentsRouter);
